c133701369989e86470018c0bbbb113fa031ccc2
borisey <slamsnv@gmail.com>
2024-11-26 19:14:16 +0300

    fix

--- tests/HelloWorldTest.php
+++ tests/HelloWorldTest.php
@@ -4,7 +4,13 @@ use PHPUnit\Framework\TestCase;
 
 final class HelloWorldTest extends TestCase
 {
-    public function testGreeting() {
+    public function testResponseCode()
+    {
+        $this->assertEquals(200, http_response_code());
+    }
+
+    public function testGreeting()
+    {
 
         $greeting = "Hello world";
         $requiredGreeting = "Hello world";
